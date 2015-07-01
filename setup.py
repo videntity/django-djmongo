270b1d254e94dbc110ec6fb08bc95bd019b67fee
Alan Viars <alan.c.viars@gmail.com>
2015-07-01 15:04:05 -0400

    Bumped version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-djmongo',
-    version='0.6.5',
+    version='0.6.6',
     packages=['djmongo', 'djmongo.console',  
               'djmongo.management.commands',
               'djmongo.management', 
