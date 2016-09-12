42e293e42b53687b716d8fba08ff5bda44fbde5a
Alan Viars <aviars@videntity.com>
2016-09-12 14:56:48 -0400

    setup file fix

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-djmongo',
-    version='0.7.5.8',
+    version='0.7.5.9',
     packages=['djmongo', 'djmongo.console',
               'djmongo.accounts', 'djmongo.accounts.migrations',
               'djmongo.aggregations', 'djmongo.aggregations.migrations',
