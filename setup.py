e3e41ca25e41644384c402cdc9c33fd8a4b27642
Alan Viars <aviars@videntity.com>
2016-10-23 23:29:49 -0400

    bumped version

--- setup.py
+++ setup.py
@@ -9,7 +9,7 @@ os.chdir(os.path.normpath(os.path.join(os.path.abspath(__file__), os.pardir)))
 
 setup(
     name='django-djmongo',
-    version='0.7.6.2',
+    version='0.7.6.3',
     packages=['djmongo', 'djmongo.console', 'djmongo.console.migrations',
               'djmongo.accounts', 'djmongo.accounts.migrations',
               'djmongo.aggregations', 'djmongo.aggregations.migrations',
