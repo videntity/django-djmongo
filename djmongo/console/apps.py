3bfb6e368f91d1d7b4d3a969037a7eabffee63a0
Alan Viars <aviars@videntity.com>
2016-12-22 05:53:27 -0500

    Updating apps files.

--- djmongo/console/apps.py
+++ djmongo/console/apps.py
@@ -3,5 +3,5 @@ from django.apps import AppConfig
 
 class DJMConsoleConfig(AppConfig):
     name = 'djmongo.console'
-    lablel = 'console'
+    lablel = 'djmongo_console'
     verbose_name = "Djmongo Archive Collection"
